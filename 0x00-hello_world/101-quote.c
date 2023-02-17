12f3244faeaae04e48c4b647a52d40e51ffbbf1e
rashidleonard <rashidleo17@gmail.com>
2023-02-16 19:00:08 -0800

    updates

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
+#include <unistd.h>
 
-int main(void)
-{
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (1);
-}
+int main(void) {
+    char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, str, 59);
+    return 1
